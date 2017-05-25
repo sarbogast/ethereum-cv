6d11ba29737419921842d0772aece1c492e6fa82
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2017-05-25 12:19:28 +0200

    Trying a github pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
      entry: './app/javascripts/app.js',
      output: {
-          path: path.resolve(__dirname, 'build'),
+          path: path.resolve(__dirname, 'docs'),
           filename: 'app.js'
      },
      plugins: [
