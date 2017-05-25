cad459a74fa6da3a04d803e48d7ed494595f4d65
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2017-05-25 13:11:31 +0200

    Trying a github pages deployment

--- truffle.js
+++ truffle.js
@@ -2,11 +2,24 @@
 require('babel-register')
 
 module.exports = {
-  networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*' // Match any network id
+    networks: {
+        development: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*' // Match any network id
+        },
+        live: {
+            host: 'localhost', // Random IP for example purposes (do not use)
+            port: 8545,
+            network_id: 1,        // Ethereum public network
+            from: '0x1494cb5f7ad7b09e9daf1ffa3070e69ae936ca0e',
+            gas: 2000000
+            // optional config values:
+            // gas
+            // gasPrice
+            // from - default address to use for any transaction Truffle makes during migrations
+            // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
+            //          - if specified, host and port are ignored.
+        }
     }
-  }
 }
